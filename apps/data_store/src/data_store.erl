63bac1babedaa8f5b6e98303e4c5047f85d72adc
Joel Borggren-Franck <joel.borggren.franck@gmail.com>
2016-03-12 19:10:51 +0100

    Store all data in list in state

--- apps/data_store/src/data_store.erl
+++ apps/data_store/src/data_store.erl
@@ -4,7 +4,7 @@
 
 %% API
 -export([start_link/1, start_link/0, start/1,
-         start/0, stop/0, add_data/1
+         start/0, stop/0, add_data/1, get_data/1
         ]).
 
 %% gen_server Callback
@@ -33,24 +33,47 @@ stop() ->
 add_data(DataPoint) ->
     gen_server:cast(?SERVER, {add_data, DataPoint}).
 
+get_data(Key) ->
+    gen_server:call(?SERVER, {get_data, Key}).
+
 init([]) ->
     %Tab = ets:new(sensor_dir_table, [{keypos,2}]),
     {ok, #state{tabs=orddict:new()}}.
 
-handle_cast({add_data, {Sensor, Type, _Value, _Time}}, State) ->
-    Dict = State#state.tabs,
-    {_Bucket, NewState} = case orddict:find({Sensor, Type}, Dict) of
-                 {ok, Bkt} -> {Bkt, State};
-                 error -> NewBucket = [],
-                          NewDict = orddict:store({Sensor, Type}, NewBucket, Dict),
-                          {NewBucket, State#state{tabs=NewDict}}
-                         end,
+handle_cast({add_data, Data = {Sensor, Type, _Value, _Time}}, State) ->
+    Buckets = State#state.tabs,
+    OldBucket = get_or_create_bucket({Sensor, Type}, Buckets),
+    NewBucket = bucket_add_data(OldBucket, Data),
+    NewBuckets = store_bucket(Buckets, {Sensor, Type}, NewBucket),
+    NewState = State#state{tabs=NewBuckets},
     {noreply, NewState};
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
+get_or_create_bucket(Key, Buckets) ->
+    Bucket = orddict:find(Key, Buckets),
+    case Bucket of
+        error -> [];
+        {ok, B} -> B
+    end.
+
+bucket_add_data(OldBucket, Data) ->
+    [Data | OldBucket].
+
+store_bucket(Buckets, Key, Bucket) ->
+    orddict:store(Key, Bucket, Buckets).
+
+handle_call({get_data, Key}, _From, State = #state{tabs = T}) ->
+    Bucket = orddict:find(Key, T),
+    case Bucket of
+        {ok, B} -> {reply, get_data2(B), State};
+        error -> {reply, [], State}
+    end;
+
 handle_call(_Unknown, _From, State) ->
     {reply, ok, State}.
+
+get_data2(Bucket) -> Bucket.
 
 handle_info(_Unknown, State) ->
     {noreply, State}.
