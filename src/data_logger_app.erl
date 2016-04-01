8c690357963ef6c2cd022b892106002d5bb5b104
Joel Borggren-Franck <joel.borggren.franck@gmail.com>
2016-04-01 22:15:26 +0200

    Clean up yaws start

--- src/data_logger_app.erl
+++ src/data_logger_app.erl
@@ -8,19 +8,19 @@
     ]).
 
 start(_Type, _StartArgs) ->
-    DL = case data_logger_sup:start_link() of
-        {ok, Pid} -> {ok, Pid};
+    case data_logger_sup:start_link() of
+        {ok, Pid} -> Yaws = maybe_start_yaws(application:get_env(data_logger, start_yaws)),
+            {ok, Pid, Yaws};
         Other -> {error, Other}
-    end,
-    ok = maybe_start_yaws(application:get_env(data_logger, start_yaws)),
-    DL.
+    end.
 
-maybe_start_yaws({ok, false}) -> ok;
+maybe_start_yaws({ok, false}) -> not_started;
 maybe_start_yaws({ok, true}) ->
-    yaws:start_embedded(get_yaws_dir(),
+    ok = yaws:start_embedded(get_yaws_dir(),
                         [servername(), listen(), port()],
                         [],
-                        get_id()).
+                        get_id()),
+    yaws_started.
 
 get_yaws_dir() ->
     {ok, Dir} = application:get_env(data_logger, yaws_www_root),
