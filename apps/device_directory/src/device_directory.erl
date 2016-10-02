8383edf61e1676333bd810b419bacefe4cca6000
Joel Borggren-Franck <joel.borggren.franck@gmail.com>
2016-10-02 21:08:32 +0200

    Make device list available

--- apps/device_directory/src/device_directory.erl
+++ apps/device_directory/src/device_directory.erl
@@ -1,8 +1,11 @@
 -module(device_directory).
 -behaviour(gen_server).
 
+-compile({parse_transform, lager_transform}).
+
 %% API.
 -export([start_link/0]).
+-export([list_devices/1]).
 
 %% gen_server.
 -export([init/1]).
@@ -12,8 +15,6 @@
 -export([terminate/2]).
 -export([code_change/3]).
 
--export([parse_devices/0]).
-
 -record(state, {devices}).
 
 %% API.
@@ -22,17 +23,26 @@
 start_link() ->
   gen_server:start_link(?MODULE, [], []).
 
+-spec list_devices(pid()) -> [_].
+list_devices(Pid) ->
+  gen_server:call(Pid, list_devices).
+
 %% gen_server.
 
 init([]) ->
-  {ok, #state{}}.
+  {ok, #state{}, 0}.
 
+handle_call(list_devices, _From, S=#state{devices=Devs}) ->
+  {reply, Devs, S};
 handle_call(_Request, _From, State) ->
   {reply, ignored, State}.
 
 handle_cast(_Msg, State) ->
   {noreply, State}.
 
+handle_info(timeout, #state{devices=undefined}) ->
+  NewState = #state{devices=parse_devices()},
+  {noreply, NewState};
 handle_info(_Info, State) ->
   {noreply, State}.
 
@@ -53,7 +63,7 @@ parse_devices() -> decode(get_devices()).
 
 get_devices() ->
   CMD = "tdtool --list-devices | awk 'BEGIN {FS=\" |=|\\t\"} NR == 1 {printf(\"[ \")} NR > 1 {printf(\"\\n ,\")}{printf(\" {device, %s, \\\"%s\\\"}\", $4,$6)} END {printf(\" ].\\n\")}'",
-  %io:format("~s~n", [CMD]).
+  lager:info("devices found: " ++ CMD),
   os:cmd(CMD).
 
 %% vim: tabstop=2 shiftwidth=2 expandtab
